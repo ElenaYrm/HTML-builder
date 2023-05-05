be26f7ba39401d9b2041156f4bdc7bc0f9b82957
Elena Yaroma <yaroma.elena@gmail.com>
2023-05-05 12:09:50 +0400

    feat: solve task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require('path');
+const { mkdir, copyFile, readdir, unlink } = require('node:fs/promises');
+const { stdout } = process;
 
+const existFolderPath = path.join(__dirname, 'files');
+const newFolderPath = path.join(__dirname, 'files-copy');
+
+async function getData(existPath, targetPath) {
+  try {
+    await mkdir(targetPath, { recursive: true });
+    const prevFiles = await readdir(targetPath, {  withFileTypes: true});
+    if (prevFiles) {
+      for (const file of prevFiles) {
+        await unlink(path.join(targetPath, file.name));
+      }
+    }
+
+    const files = await readdir(existPath, {  withFileTypes: true});
+    for (const file of files) {
+      await copyFile(path.join(existPath, file.name), path.join(targetPath, file.name));
+    }
+  } catch (error) {
+    stdout.write(error.message);
+  }
+}
+
+getData(existFolderPath, newFolderPath);
